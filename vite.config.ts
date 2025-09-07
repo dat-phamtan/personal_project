67873a8f09f29c18403eb82428b060912253aed6
dat-phamtan <dat.phamtan@hcmut.edu.vn>
2025-09-07 17:19:14 +0700

    vite.config.ts fixed

--- vite.config.ts
+++ vite.config.ts
@@ -9,10 +9,14 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 3000,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react(), mode === "development" && componentTagger()].filter(
+    Boolean
+  ),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  // 👇 thêm dòng này
+  base: "/personal_project/",
 }));
